246853f3382621e20a648a584e63158a8217df33
Alexander Griffin <alexander_goudjeb@hotmail.com>
2023-07-09 16:13:09 +0200

    Add AudioVisualizer.

--- BGAnimations/OFSelectMusic background.lua
+++ BGAnimations/OFSelectMusic background.lua
@@ -6,5 +6,13 @@ return Def.ActorFrame {
 		InitCommand = function(self)
 			TF_WHEEL.BG = self
 		end
+	},
+	Def.AudioVisualizer {
+		Name="AV",
+		UpdateRate = 0.05,
+		Amount = 68,
+		OnCommand = function(self)
+			self:Center():diffuse(1,1,1,.25):zoomx(SCREEN_HEIGHT/360):zoomy(100)
+		end
 	}
 }
